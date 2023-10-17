4b736d03bdf3154d53f57cc80ee935981a6f4e77
BernardoBWeiss <142499089+BernardoBWeiss@users.noreply.github.com>
2023-10-17 14:22:53 +0000

    a

---
+++ js/script.js
@@ -0,0 +1,15 @@
+alert("Olá Mundo");
+function trocarfrase() {
+    var fraseatual = document.getElementById("frase");
+    var elemento = getElementById;
+    fraseAtual.className = h1_2;
+    elemento.innerHTML = "funcionou, a frase mudou";
+    elemento.style.color = "red";
+    elemento.style.fontsize = "25px";
+    elemento.style.backgroundColor = "blue";
+    document.body.style.backgroundColor = "yellow";
+    elemento.innerHTML = "<img scr='img/     ' />";
+    function trocarClasse() { var trocarClasse; }
+}
+var nome = "Bernardo";
+console.log("o nome é" + nome);
